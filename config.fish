3c05f7ddc02bbd94183f3d3471151fbc4b456fc0
iampeterbanjo <peterbanjo@gmail.com>
2015-12-02 10:22:16 +0000

    Create config.fish
    Configuration for http://fishshell.com/

---
+++ config.fish
@@ -0,0 +1,21 @@
+alias vim               mvim
+alias brackets          "open -a Brackets"
+
+alias editfishconfig    'vim ~/.config/fish/config.fish'
+alias showfishconfig    'cat ~/.config/fish/config.fish'
+alias reloadfishconfig  'source ~/.config/fish/config.fish'
+
+alias gitadd            'git add -A'
+alias gitcm             'git commit -am'
+alias gitd              'git diff'
+alias gits              'git status'
+alias gitch             'git checkout'
+alias gitcob            'git checkout -br'
+alias gitpush           'git push -u --all'
+
+alias vtstat            'vagrant status'
+alias vtup              'vagrant up'
+alias vthalt            'vagrant halt'
+alias vtreload          'vagrant reloadfishconfig'
+
+alias cd-homestead      'cd ~/Projects/homestead/'
