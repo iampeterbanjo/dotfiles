3be1e9b7d1806bdd8fdc17f956ce52ee76d511f6
iampeterbanjo <peterbanjo@gmail.com>
2015-12-03 09:34:24 +0000

    Add comments and VS Code Editor

--- config.fish
+++ config.fish
@@ -1,9 +1,22 @@
+#
+# EDITORS
+#
+
 alias vim               mvim
-alias brackets          "open -a Brackets"
+alias brackets          'open -a Brackets'
+alias code              'open -n -b "com.microsoft.VSCode"'
+
+#
+# MAINTENANCE
+#
 
 alias editfishconfig    'vim ~/.config/fish/config.fish'
 alias showfishconfig    'cat ~/.config/fish/config.fish'
 alias reloadfishconfig  'source ~/.config/fish/config.fish'
+
+#
+# GIT
+#
 
 alias gitadd            'git add -A'
 alias gitcm             'git commit -am'
@@ -13,9 +26,17 @@ alias gitch             'git checkout'
 alias gitcob            'git checkout -br'
 alias gitpush           'git push -u --all'
 
+#
+# VAGRANT
+#
+
 alias vtstat            'vagrant status'
 alias vtup              'vagrant up'
 alias vthalt            'vagrant halt'
 alias vtreload          'vagrant reloadfishconfig'
+
+#
+# DIRECTORIES
+#
 
 alias cd-homestead      'cd ~/Projects/homestead/'
