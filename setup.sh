6a05721fa6c8f210ccca62f6d2f0fb2e4a2ba7b6
Peter Banjo <peterbanjo@gmail.com>
2018-01-24 07:27:35 +0000

    docker and docker-compose installation steps

--- setup.sh
+++ setup.sh
@@ -86,6 +86,22 @@ apt://arc-theme
 # Fira Code
 https://github.com/tonsky/FiraCode
 
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+# check docker status
+sudo systemctl status docker
+# run docker without sudo
+sudo usermod -aG docker ${USER}
+su - ${USER}
+
+# docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
 # variables
 export PROJECTS=/media/iampeterbanjo/data/projects
 export GOROOT=$HOME/go
