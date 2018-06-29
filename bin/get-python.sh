e8251b281d662c3138363d104d6c2a0180f41f6e
iampeterbanjo <peterbanjo@gmail.com>
2018-06-29 21:37:10 +0100

    fix python install

--- bin/get-python.sh
+++ bin/get-python.sh
@@ -3,3 +3,5 @@
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev python3-setuptools
+
+sudo apt-get install python-setuptools python-pip
