b01e986fe9cca17b623613154655cd91c10a0bc7
iampeterbanjo <peterbanjo@gmail.com>
2018-07-30 06:04:56 +0100

    fix env setting

--- bin/get-flash.sh
+++ bin/get-flash.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
 sudo apt-get update
 sudo apt-get install adobe-flashplugin
--- bin/rm-flash.sh
+++ bin/rm-flash.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 sudo apt remove adobe-flashplugin adobe-flash-properties-gtk
