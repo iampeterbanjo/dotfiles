186f43526c9bf0d540c82c981be8b64cfdf8ea0d
iampeterbanjo <peterbanjo@gmail.com>
2018-09-03 20:44:28 +0100

    add datastore and python to google sdk script

--- bin/get-google-cloud-sdk.sh
+++ bin/get-google-cloud-sdk.sh
@@ -8,5 +8,5 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee 
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
-sudo apt-get install google-cloud-sdk-app-engine-go
+sudo apt-get install google-cloud-sdk-app-engine-go google-cloud-sdk-app-engine-python google-cloud-sdk-datastore-emulator
 gcloud init
