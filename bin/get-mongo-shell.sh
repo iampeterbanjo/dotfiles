3ab7befbfd96a6d6870a9ac99ccd79aaf6809da5
Peter Banjo <peterbanjo@gmail.com>
2018-12-07 16:18:16 +0000

    Create get-mongo-shell.sh

---
+++ bin/get-mongo-shell.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+version=4.0.4
+baseUrl='https://downloads.mongodb.org/linux'
+file="mongodb-shell-linux-x86_64-ubuntu1604-$version.tgz"
+install="$HOME/programs"
+destination="$HOME/Downloads"
+
+wget "$baseUrl/$file" -N -P "$destination"
+downloaded_file="$destination/$file"
+
+mkdir -p $HOME/programs/mongodb-shell
+
+tar -xvf "$downloaded_file" -C $HOME/programs/mongodb-shell --strip-components=1
+
+if grep -q MONGO_SHELL "$HOME/.bashrc"; then
+  echo 'No update to PATH'
+else
+  echo 'Updating PATH'
+
+  echo 'export MONGO_SHELL=$HOME/programs/mongodb-shell' >> ~/.bashrc
+  echo 'export PATH=$MONGO_SHELL:$PATH' >> ~/.bashrc
+  echo '' >> ~/.bashrc
+fi
+
+echo "done"
