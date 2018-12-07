163361b80007d0ce1d7a595f075c86375a18d6db
Peter Banjo <peterbanjo@gmail.com>
2018-12-07 09:44:08 +0000

    Create get-mongodb.sh

---
+++ bin/get-mongodb.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# install mongodb for ubuntu
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -c -s)/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
