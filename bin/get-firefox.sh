4bb0ee3f89bccd9421fb32c9600d0962c063905d
Peter Banjo <peterbanjo@gmail.com>
2018-12-14 08:35:37 +0000

    Create get-firefox.sh

---
+++ bin/get-firefox.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# install regular firefox from https://launchpad.net/~ubuntu-mozilla-security/+archive/ubuntu/ppa
+sudo add-apt-repository ppa:ubuntu-mozilla-security/ppa
+sudo apt-get update
+sudo apt install firefox
