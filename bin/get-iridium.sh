933f9b68570a6f45a879f3d8e51be17be8362cde
iampeterbanjo <peterbanjo@gmail.com>
2019-05-16 09:13:03 +0100

    fix exec

--- bin/get-iridium.sh
+++ bin/get-iridium.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 wget -qO - https://downloads.iridiumbrowser.de/ubuntu/iridium-release-sign-01.pub|sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/iridium-browser.list
 deb [arch=amd64] https://downloads.iridiumbrowser.de/deb/ stable main
