a4925ff5bb53e44a24fd2be19cfaa4e31dfb16ad
iampeterbanjo <peterbanjo@gmail.com>
2019-05-16 06:06:50 +0100

    add iridium browser install

---
+++ bin/get-iridium.sh
@@ -0,0 +1,7 @@
+wget -qO - https://downloads.iridiumbrowser.de/ubuntu/iridium-release-sign-01.pub|sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/iridium-browser.list
+deb [arch=amd64] https://downloads.iridiumbrowser.de/deb/ stable main
+#deb-src https://downloads.iridiumbrowser.de/deb/ stable main
+EOF
+sudo apt-get update
+sudo apt-get install iridium-browser
