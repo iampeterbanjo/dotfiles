61e8ae2ee437757f7dd16ecd51149edce02444ff
iampeterbanjo <peterbanjo@gmail.com>
2019-05-18 19:27:51 +0100

    update vagrant install script

--- bin/get-vagrant.sh
+++ bin/get-vagrant.sh
@@ -3,5 +3,17 @@
 # install vagrant
 sudo apt update
 sudo apt install virtualbox
-sudo apt install vagrant
+
+version=2.2.4
+baseUrl="https://releases.hashicorp.com/vagrant/$version"
+file="vagrant_"
+file+="$version"
+file+="_linux_amd64.zip"
+
+. ./bin/help-get-exec.sh
+
+help_get "$baseUrl" "$file"
+
+echo 'done'
+
 vagrant --version
