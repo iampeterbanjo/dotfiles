eae720ee7a20a643f971a2f0fc0620da3be48107
iampeterbanjo <iampeterbanjo@users.noreply.github.com>
2019-11-07 11:45:25 +0000

    update and refactor elixir/erlang install

--- bin/get-elixir.sh
+++ bin/get-elixir.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-asdf install elixir 1.7.4
-asdf global elixir 1.7.4
+version=1.9.2
+asdf install elixir "$version"
+asdf global elixir "$version"
--- bin/get-erlang.sh
+++ bin/get-erlang.sh
@@ -4,6 +4,7 @@ sudo apt-get autoremove automake autoconf
 sudo apt-get install automake autoconf
 
 # install erlang
+version=21.1.1
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-asdf install erlang 21.1.1
-asdf global erlang 21.1.1
+asdf install erlang "$version"
+asdf global erlang "$version"
