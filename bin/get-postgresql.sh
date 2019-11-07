04145ccfd81b886aafea49f4bf17e20fac8dd6cf
iampeterbanjo <iampeterbanjo@users.noreply.github.com>
2019-11-07 11:35:14 +0000

    postgres install for linux and mac os

--- bin/get-postgresql.sh
+++ bin/get-postgresql.sh
@@ -1,10 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/env fish
 
 # install postgresql db
-sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib
+switch (uname)
+  case Linux
+    sudo apt-get update
+    sudo apt-get install postgresql postgresql-contrib
 
-echo 'postgresql installed'
-echo 'create a new user'
+    echo 'postgresql installed on Linux'
 
-sudo -u postgres createuser --interactive
+    sudo -u postgres createuser --interactive
+  case Darwin
+    brew install postgresql
+    ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+    brew services start postgresql
+
+    echo 'postgresql installed on MacOS'
+    echo 'create user with'
+    echo '$> psql'
+    echo '$> createuser --interactive'
+end
