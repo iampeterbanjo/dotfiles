d87510fc155e58798455cae25718c87bd0a69a4f
iampeterbanjo <gitblame@iampeterbanjo.com>
2020-01-08 15:18:02 +0000

    install fish deps before install

--- bin/get-fish-config.sh
+++ bin/get-fish-config.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# add dependencies first
+brew install rbenv direnv
+
+# get fish config
 mv ~/.config/fish ~/.config/fish_backup
 cd ~/.config/
 git clone git@bitbucket.org:iampeterbanjo/fish.git
