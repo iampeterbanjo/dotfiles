b2205cfb7cdde32291bd499ae78178df4820f1c4
Peter Banjo <221853+iampeterbanjo@users.noreply.github.com>
2020-04-10 08:30:07 +0000

    use bashrc

--- bin/get-brew.sh
+++ bin/get-brew.sh
@@ -5,6 +5,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/i
 
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+test -r ~/.bashrc && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
 
