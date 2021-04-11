5939f1a3c4369adbb3d16b48adbc168d8e67d94d
iampeterbanjo <gitblame@iampeterbanjo.com>
2021-04-11 14:24:11 +0100

    refactor k3d. create local registry

--- bin/get-k3d.sh
+++ bin/get-k3d.sh
@@ -1,3 +1,11 @@
 #! /usr/bin/env bash
 
 brew install k3d
+
+CLUSTER_NAME='local-cattle-ranch'
+
+k3d cluster create "$CLUSTER_NAME" --registry-create
+
+# set k8s current context
+k3d kubeconfig merge "$CLUSTER_NAME" --kubeconfig-switch-context
+kubectl get pods --all-namespaces
