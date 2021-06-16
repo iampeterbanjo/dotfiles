fea988b181182a7af8100101050ba51043d5fd73
Peter Banjo <me@iampeterbanjo.com>
2021-06-16 07:26:06 +0000

    configure vscode as git mergetool

--- bin/get-git.sh
+++ bin/get-git.sh
@@ -4,3 +4,9 @@ sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update
 sudo apt-get install git
 sudo apt-get install git-extras
+
+# configure vscode as git mergetool
+git config --global merge.tool vscode
+git config --global mergetool.vscode.cmd "code --wait $MERGED"
+git config --global diff.tool vscode
+git config --global difftool.vscode.cmd "code --wait --diff $LOCAL $REMOTE"
